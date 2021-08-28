38283a5e18a63c0122f9ddf40a7abbb1afeb6c2f
Dilrose Reji <dilrose2000@gmail.com>
2021-08-28 17:42:57 +0530

    updated

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from sqlite3 import *
+import pickle
 
 app = Flask(__name__)
 app.secret_key = 'lungcancerdetection'
@@ -80,6 +81,107 @@ def main():
 def logout():
 	session.pop('uname',None)
 	return redirect(url_for('login'))
+
+@app.route('/check', methods=["POST","GET"])
+def check():
+    if request.method == 'POST':
+        age = request.form['age']
+        r2 = request.form['r2']
+        if r2 == 'yes':
+            smoke = '1'
+        else:
+            smoke = '0'
+
+        r3 = request.form['r3']
+        if r3 == 'yes':
+            yf = '1'
+        else:
+            yf = '0'
+        
+        r4 = request.form['r4']
+        if r4 == 'yes':
+            a = '1'
+        else:
+            a = '0'
+
+        r5 = request.form['r5']
+        if r5 == 'yes':
+            pp = '1'
+        else:
+            pp = '0'
+
+        r6 = request.form['r6']
+        if r6 == 'yes':
+            cd = '1'
+        else:
+            cd = '0'
+        
+        r7 = request.form['r7']
+        if r7 =="yes":
+            f = '1'
+        else:
+            f = '0'
+        
+        r8 = request.form['r8']
+        if r8 =='yes':
+            allergy = '1'
+        else:
+            allergy = '0'
+        
+        r9 = request.form['r9']
+        if r9 == 'yes':
+            wheezing = '1'
+        else:
+            wheezing = '0'
+
+        r10 = request.form['r10']
+        if r10 == 'yes':
+            alch = '1'
+        else:
+            alch = '0'
+        
+        r11 = request.form['r11']
+        if r11 == 'yes':
+            cough = '1'
+        else:
+            cough = '0'
+
+        r12 = request.form['r12']
+        if r12 == 'yes':
+            sob = '1'
+        else:
+            sob = '0'
+
+        r13 = request.form['r13']
+        if r13 == 'yes':
+            swallow = '1'
+        else:
+            swallow = '0'
+
+        r14 = request.form['r14']
+        if r14 == 'yes':
+            cp = '1'
+        else:
+            cp = '0'
+        
+        r15 = request.form['r15']
+        if r15 == 'male':
+            gender = '1'
+        else:
+            gender = '0'
+        
+        d = [[age,smoke,yf,a,pp,cd,f,allergy,wheezing,alch,cough,sob,swallow,cp,gender]]
+        with open('db.model', 'rb') as f:
+            model = pickle.load(f)
+        res = model.predict(d)
+        res = str(res)
+        return render_template('main.html', msg=res)
+
+    else:
+        return render_template('main.html')
+
+
+
 
 
 
