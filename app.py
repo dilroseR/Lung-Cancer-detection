38f87a58abe4446ea2c6dd890828d5f1d6ed0f72
Dilrose Reji <dilrose2000@gmail.com>
2021-08-30 16:13:54 +0530

    updated

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from sqlite3 import *
+from flask_mail import Mail, Message
 import pickle
 
 app = Flask(__name__)
+app.config['MAIL_SERVER'] = "smtp.gmail.com"
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'dreji1234@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Abc45678*'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
+mail = Mail(app)
 app.secret_key = 'lungcancerdetection'
 
 @app.route('/', methods=['POST','GET'])
@@ -85,97 +94,137 @@ def logout():
 @app.route('/check', methods=["POST","GET"])
 def check():
     if request.method == 'POST':
-        age = request.form['age']
+        ls=[]
+        age = int(request.form['age'])
+        ls.append(age)
+
         r2 = request.form['r2']
         if r2 == 'yes':
             smoke = '1'
+            ls.append("Yes")
         else:
             smoke = '0'
+            ls.append("No")
 
         r3 = request.form['r3']
         if r3 == 'yes':
             yf = '1'
+            ls.append("Yes")
         else:
             yf = '0'
+            ls.append("No")
         
         r4 = request.form['r4']
         if r4 == 'yes':
             a = '1'
+            ls.append("Yes")
         else:
             a = '0'
+            ls.append("No")
 
         r5 = request.form['r5']
         if r5 == 'yes':
             pp = '1'
+            ls.append("Yes")
         else:
             pp = '0'
+            ls.append("No")
 
         r6 = request.form['r6']
         if r6 == 'yes':
             cd = '1'
+            ls.append("Yes")
         else:
             cd = '0'
+            ls.append("No")
         
         r7 = request.form['r7']
         if r7 =="yes":
             f = '1'
+            ls.append("Yes")
         else:
             f = '0'
+            ls.append("No")
         
         r8 = request.form['r8']
         if r8 =='yes':
             allergy = '1'
+            ls.append("Yes")
         else:
             allergy = '0'
+            ls.append("No")
         
         r9 = request.form['r9']
         if r9 == 'yes':
             wheezing = '1'
+            ls.append("Yes")
         else:
             wheezing = '0'
+            ls.append("No")
 
         r10 = request.form['r10']
         if r10 == 'yes':
             alch = '1'
+            ls.append("Yes")
         else:
             alch = '0'
+            ls.append("No")
         
         r11 = request.form['r11']
         if r11 == 'yes':
             cough = '1'
+            ls.append("Yes")
         else:
             cough = '0'
+            ls.append("No")
 
         r12 = request.form['r12']
         if r12 == 'yes':
             sob = '1'
+            ls.append("Yes")
         else:
             sob = '0'
+            ls.append("No")
 
         r13 = request.form['r13']
         if r13 == 'yes':
             swallow = '1'
+            ls.append("Yes")
         else:
             swallow = '0'
+            ls.append("No")
 
         r14 = request.form['r14']
         if r14 == 'yes':
             cp = '1'
+            ls.append("Yes")
         else:
             cp = '0'
+            ls.append("No")
         
         r15 = request.form['r15']
         if r15 == 'male':
             gender = '1'
+            ls.append("Male")
         else:
             gender = '0'
-        
+            ls.append("Female")
+
+        em = request.form['em']
+        print(ls)
         d = [[age,smoke,yf,a,pp,cd,f,allergy,wheezing,alch,cough,sob,swallow,cp,gender]]
         with open('db.model', 'rb') as f:
             model = pickle.load(f)
         res = model.predict(d)
-        res = str(res)
-        return render_template('main.html', msg=res)
+        msg = Message('RESULTS!', sender='dreji1234@gmail.com', recipients=[em])
+        if res[0] == 'NO':
+            msg.html = "<h2 style='color:red;'>You don't have any symptoms of lung cancer.</h2><h4>Stay Healthy!</h4>"
+                 
+        else:
+            msg.html = "<h2 style='color:red;'>You are likely to have lung cancer.</h2><br><h4>Kindly visit a doctor soon!</h4>"
+        
+        mail.send(msg)
+        return render_template('main.html', msg=1)
 
     else:
         return render_template('main.html')
